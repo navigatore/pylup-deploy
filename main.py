a948c0eeac3b12ce0d9124e73c6fd45ce8c73a91
Wojciech Chlapek <wojciech.chlapek@tlen.pl>
2020-04-05 01:21:34 +0200

    Exercise 1

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get('/')
+def hello_world():
+    return {"message": "Hello World during the coronavirus pandemic!"}
