8db9e6b9bf5c200eb59ddc4e8a4c3a3f869763d8
Wojciech Chlapek <wojciech.chlapek@tlen.pl>
2020-04-05 02:00:17 +0200

    Exercise 2

--- main.py
+++ main.py
@@ -3,6 +3,31 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class MethodResp(BaseModel):
+    method: str
+
+
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+
+@app.get('/method')
+def method_get():
+    return MethodResp(method="GET")
+
+
+@app.post('/method')
+def method_post():
+    return MethodResp(method="POST")
+
+
+@app.delete('/method')
+def method_delete():
+    return MethodResp(method="DELETE")
+
+
+@app.put('/method')
+def method_put():
+    return MethodResp(method="PUT")
