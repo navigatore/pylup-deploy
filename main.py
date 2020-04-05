35debfce2fec16566199b6cb4d9f9b1672f0e27f
Wojciech Chlapek <wojciech.chlapek@tlen.pl>
2020-04-05 15:24:03 +0200

    Exercise 3

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ class MethodResp(BaseModel):
     method: str
 
 
+class PatientName(BaseModel):
+    name: str
+    surename: str
+
+
+class PatientResp(BaseModel):
+    id: int
+    patient: PatientName
+
+
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
@@ -31,3 +41,12 @@ def method_delete():
 @app.put('/method')
 def method_put():
     return MethodResp(method="PUT")
+
+
+@app.post('/patient')
+def patient(patient_name: PatientName):
+    patient.ctr += 1
+    return PatientResp(id=patient.ctr, patient=patient_name)
+
+
+patient.ctr = -1
