9564bf046cf0c2477e2dc96ae9fb4c8e838578ec
Wojciech Chlapek <wojciech.chlapek@tlen.pl>
2020-04-26 23:15:16 +0200

    Add welcome endpoint

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = FastAPI()
 
 patients = {}
 
+
 class MethodResp(BaseModel):
     method: str
 
@@ -19,41 +20,41 @@ class PatientResp(BaseModel):
     patient: PatientName
 
 
-@app.get('/')
+@app.get("/")
+@app.get("/welcome")
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 
-@app.get('/method')
+@app.get("/method")
 def method_get():
     return MethodResp(method="GET")
 
 
-@app.post('/method')
+@app.post("/method")
 def method_post():
     return MethodResp(method="POST")
 
 
-@app.delete('/method')
+@app.delete("/method")
 def method_delete():
     return MethodResp(method="DELETE")
 
 
-@app.put('/method')
+@app.put("/method")
 def method_put():
     return MethodResp(method="PUT")
 
 
-@app.post('/patient')
+@app.post("/patient")
 def patient(patient_name: PatientName):
     new_id = len(patients)
     patients[new_id] = patient_name
     return PatientResp(id=new_id, patient=patient_name)
 
 
-@app.get('/patient/{pk}')
+@app.get("/patient/{pk}")
 def get_patient(pk: int):
     if pk not in patients:
         raise HTTPException(status_code=204, detail="Nonexisting patient ID")
     return patients[pk]
-
